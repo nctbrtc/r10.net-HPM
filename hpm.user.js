40685334b3c2878653dc6aa9baf4f447ed68e454
nctbrtc <41221911+nctbrtc@users.noreply.github.com>
2021-01-06 23:16:58 -0500

    Update hpm.user.js

--- hpm.user.js
+++ hpm.user.js
@@ -9,38 +9,41 @@
 // ==/UserScript==
 
 // 12.23.2020 Yeni tema ile gelen buyuk header i kucultuyoruz :
-
-document.querySelector("body > header").style.zoom="0.8";
-	document.querySelector("body > header > div.headerTop").style.zoom='0.8';
-	document.querySelector("body > header").style.marginTop="-2%";
-	document.querySelector("body > header > div.headerTop > div > div.user > div.icon.messages > div.notificationList").style.zoom="1.5";
-	document.querySelector("body > header > div.headerTop > div > div.user > div.icon.notify > div.notificationList > div.list").style.zoom="1.5";
+document.querySelector("body > header").style.zoom = "0.8";
+document.querySelector("body > header > div.headerTop").style.zoom = '0.8';
+document.querySelector("body > header").style.marginTop = "-2%";
+document.querySelector("body > header > div.headerTop > div > div.user > div.icon.messages > div.notificationList").style.zoom = "1.5";
+document.querySelector("body > header > div.headerTop > div > div.user > div.icon.notify > div.notificationList > div.list").style.zoom = "1.5";
 
 
 var url = window.location.href;
 var n = url.includes("private.php");
 var konumu = url.includes(".html");
 
-[].forEach.call(document.querySelectorAll('img'), function (img) { if (/\.gif/i.test(img.src)) img.remove() })
+[].forEach.call(document.querySelectorAll('img'), function(img) {
+    if (/\.gif/i.test(img.src)) img.remove()
+})
 
 console.log('gif ler kaldirildi, oh ne guzel temiz hava ⛅')
 
 
 
 
-var xPathRes = document.evaluate ('/html/body/main/div[1]/div/div[1]/button/i', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+var xPathRes = document.evaluate('/html/body/main/div[1]/div/div[1]/button/i', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
 xPathRes.singleNodeValue.click();
 
-$.cookie("r10LeftHide", 1, {expires: 10000, path: "/"}); // sol sidebar reklam kapatma
+$.cookie("r10LeftHide", 1, {
+    expires: 10000,
+    path: "/"
+}); // sol sidebar reklam kapatma
 
 
 setTimeout(otoYenileyiKapat, 5000); // Otomatik Sayfa Yenilemeyi 10 saniye sonra aktifsizlestirir.
- function otoYenileyiKapat()
-{
- window.stop();
- console.log('otomatik refresh aktifsizlestirildi 🚶 ');
+function otoYenileyiKapat() {
+    window.stop();
+    console.log('otomatik refresh aktifsizlestirildi 🚶 ');
 
- }
+}
 
 if (n) {
     var hpm = url.includes("hpm=true");
@@ -52,20 +55,20 @@ if (n) {
 
 
         var hocam = "Hocam selamlar, \n [url=" + params[2] + "][B][COLOR=#D14841]" + decodeURI(params[1]) + "[/COLOR][/B][/url] konunuz icin yaziyorum. Detaylari alabilir miyim ?";
-        document.querySelector("#message_form > div.head > div:nth-child(3) > div:nth-child(2) > input").value = decodeURI(params[1]).replace(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
- + " konusu hakkinda";
+        document.querySelector("#message_form > div.head > div:nth-child(3) > div:nth-child(2) > input").value = decodeURI(params[1]).replace(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, ''); +
+        " konusu hakkinda";
         document.querySelector("#vB_Editor_001 > div > div.fr-wrapper > div").click();
         document.querySelector("#r10BBCode-1").click();
         document.querySelector("#vB_Editor_001 > div > div.fr-wrapper > div").innerText = hocam;
 
-    //  setTimeout(gonderebas, 1000); // Gonder Butonuna basar 1.5 sn icinde
+        //  setTimeout(gonderebas, 1000); // Gonder Butonuna basar 1.5 sn icinde
 
         function gonderebas() {
             document.querySelector("#vB_Editor_001_save").click();
         }
 
-         function kapanSusam() {
-           window.close();
+        function kapanSusam() {
+            window.close();
         }
 
     }
@@ -90,4 +93,3 @@ if (konumu)
         "href='" + encodeURI(hpmLink) + "'><b style='color:#ffffff'><i class='fa fa-bolt'></i> HPM </b></a></div>";
     birinci[0].appendChild(hizliPM);
 }
-
